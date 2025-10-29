b2cb378c6c353ae66291a3393bb1c79817d5bea3
RajanAxin <rajan@axinenterprise.com>
2025-10-30 04:47:45 +0530

    trasnfer call to truck rental

--- server.py
+++ server.py
@@ -857,7 +857,7 @@ async def receive_from_openai(message, plivo_ws, openai_ws, conversation_state):
                     await transfer_call(lead_id, to_number,1)
                     print("Processing Live Transfer...")
                 elif disposition_status['value'] == 'Truck Retnal Transfer' or disposition_status['value'] == 'Truck Rental Transfer':
-                    await dispostion_status_update(lead_id,'Truck Rental')
+                    #await dispostion_status_update(lead_id,'Truck Rental')
                     await transfer_call(lead_id, to_number,2)
                     # Do something for Truck Retnal Transfer
                     print("Processing Truck Retnal Transfer...")
