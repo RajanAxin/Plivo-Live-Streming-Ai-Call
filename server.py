8fdb4d9bee5859183fc5ce575db8d4e8dbba51f9
RajanAxin <rajan@axinenterprise.com>
2026-01-02 02:24:35 +0530

    ass

--- server.py
+++ server.py
@@ -2755,9 +2755,9 @@ async def set_ma_lead_dispostion_status_update(lead_id, disposition_val, t_call_
         
         # Send request to the new API
         response = requests.get(api_url, data=params)
-        api_response_text = response.text if response.text else str(response)
-        #print(f"[DISPOSITION] Response: {response}")
-        #print(f"[DISPOSITION] API Response: {api_response_text}")
+        api_response_text = response.msg if response.msg else str(response)
+        print(f"[DISPOSITION] Response: {response}")
+        print(f"[DISPOSITION] API Response: {api_response_text}")
         #print(f"[DISPOSITION] Lead {lead_id} disposition updated to {disposition}")
 
         # -------------------------------
